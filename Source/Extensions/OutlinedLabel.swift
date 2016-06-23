afe484cb5051750552bfff5c25bc05ebdc19a80c
Alexander Bekert <bekert@me.com>
2016-06-23 16:59:22 +0300

    Update graphics. Insert money.

---
+++ Source/Extensions/OutlinedLabel.swift
@@ -0,0 +1,39 @@
+//
+//  OutlinedLabel.swift
+//  Denomination
+//
+//  Created by Alexander Bekert on 23/06/16.
+//  Copyright © 2016 Alexander Bekert. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class UIOutlinedLabel: UILabel {
+    @IBInspectable
+    var outlineWidth: CGFloat = 1
+    @IBInspectable
+    var outlineColor: UIColor = UIColor.whiteColor()
+    
+    override func drawTextInRect(rect: CGRect) {
+//        self.layer.borderColor = outlineColor.CGColor
+//        self.layer.borderWidth = outlineWidth
+//        super.drawTextInRect(rect)
+        
+//        let strokeTextAttributes = [
+//            NSStrokeColorAttributeName : outlineColor,
+//            NSStrokeWidthAttributeName : -1 * outlineWidth,
+//            ]
+//        
+//        self.attributedText = NSAttributedString(string: self.text ?? "", attributes: strokeTextAttributes)
+//        super.drawTextInRect(rect)
+        
+//        self.layer.shadowColor = outlineColor.CGColor
+//        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+//        self.layer.shadowOpacity = 1.0;
+//        self.layer.shadowRadius = outlineWidth;
+//        self.layer.shouldRasterize = true
+//        self.layer.masksToBounds = false
+//        super.drawTextInRect(rect)
+    }
+}
