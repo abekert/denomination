14e6f9692bf1d4b8745090a59b38ccbebd600a33
Alexander Bekert <a.bekert@redrockapps.com>
2017-05-28 11:18:58 +0300

    Embed table view controller to container. Update status bar color

---
+++ Source/View Controllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Denomination
+//
+//  Created by Alexander Bekert on 5/28/17.
+//  Copyright © 2017 Alexander Bekert. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent;
+    }
+}
